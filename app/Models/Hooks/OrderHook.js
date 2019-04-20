8f8be9128c8a99b517542ea5675706d7d068d0a9
met4tron <yuurig@gmail.com>
2019-04-20 17:35:19 -0300

    Order hook

---
+++ app/Models/Hooks/OrderHook.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const OrderHook = (exports = module.exports = {})
+
+OrderHook.updateValues = async modelInstance => {
+  modelInstance.$sideLoaded.subtotal = await modelInstance
+    .items()
+    .getSum('subtotal')
+
+  modelInstance.$sideLoaded.qty_items = await modelInstance
+    .items()
+    .getSum('quantity')
+
+  modelInstance.$sideLoaded.discount = await modelInstance
+    .discounts()
+    .getSum('discount')
+
+  modelInstance.total =
+    modelInstance.$sideLoaded.subtotal - modelInstance.$sideLoaded.discount
+}
+
+OrderHook.updateCollectionValues = async models => {
+  for (let model of models) {
+    model = await OrderHook.updateValues(model)
+  }
+}
