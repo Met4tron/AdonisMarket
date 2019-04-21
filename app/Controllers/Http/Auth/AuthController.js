1240c572d3a67e556d326f143fc0873c0386e2d6
met4tron <yuurig@gmail.com>
2019-04-21 18:39:37 -0300

    Creation of routes and controllers :fire:

---
+++ app/Controllers/Http/Auth/AuthController.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class AuthController {
+  async register({ request, response }) {}
+  async login({ request, response, auth }) {}
+  async logout({ request, response, auth }) {}
+  async refresh({ request, response, auth }) {}
+  async forgot({ request, response }) {}
+  async remeber({ request, response }) {}
+  async reset({ request, response }) {}
+}
+
+module.exports = AuthController
